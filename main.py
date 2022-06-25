e2b6676180a7948c57a77030bf1b37ad7aadac1c
Kubases <mokwajak@gmail.com>
2022-06-25 10:41:56 +0200

    test_commit

---
+++ main.py
@@ -0,0 +1,36 @@
+def init_heading_sets():
+    heading_set1 = [{id: 1, 'title': "heading1", 'heading_level': 0},
+                    {id: 2, 'title': "heading2", 'heading_level': 2},
+                    {id: 3, 'title': "heading3", 'heading_level': 1},
+                    {id: 4, 'title': "heading4", 'heading_level': 1}]
+
+    heading_set2 = [{id: 1, 'title': "heading1", 'heading_level': 0},
+                    {id: 2, 'title': "heading2", 'heading_level': 3},
+                    {id: 3, 'title': "heading3", 'heading_level': 4},
+                    {id: 4, 'title': "heading4", 'heading_level': 1},
+                    {id: 5, 'title': "heading5", 'heading_level': 0}]
+
+    heading_set3 = [{id: 1, 'title': "heading1", 'heading_level': 0},
+                    {id: 2, 'title': "heading2", 'heading_level': 3},
+                    {id: 3, 'title': "heading3", 'heading_level': 4},
+                    {id: 4, 'title': "heading4", 'heading_level': 1},
+                    {id: 5, 'title': "heading5", 'heading_level': 0}]
+
+    return heading_set1, heading_set2, heading_set3
+
+
+def write_md(heading_set):
+    with open('test.md', 'w') as f:
+        for i in heading_set:
+            line = ''.join('#' * (i['heading_level'] + 1))
+            line = line + ' ' + i['title'] + '\n'
+            f.write(line)
+
+
+def main():
+    heading_set1, heading_set2, heading_set3 = init_heading_sets()
+    write_md(heading_set1)
+
+
+if __name__ == '__main__':
+    main()
